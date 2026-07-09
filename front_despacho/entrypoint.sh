a066ac40c48fa9068d58105bb07bb746cea75ed3
javier rojas <javie.rojasc@duocuc.cl>
2026-07-08 20:02:36 -0400

    intento 10

--- front_despacho/entrypoint.sh
+++ front_despacho/entrypoint.sh
@@ -1,6 +1,19 @@
-#!/bin/sh
+!/bin/sh
 set -eu
 
-envsubst '${BACKEND_HOST} ${BACKEND_HOST_DESPACHOS}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+# Set default values for backend hosts
+export BACKEND_HOST="${BACKEND_HOST:-backend-ventas}"
+export BACKEND_HOST_DESPACHOS="${BACKEND_HOST_DESPACHOS:-backend-despacho}"
 
-exec nginx -g 'daemon off;'
+echo "Starting frontend with BACKEND_HOST=$BACKEND_HOST and BACKEND_HOST_DESPACHOS=$BACKEND_HOST_DESPACHOS"
+
+# Generate nginx config from template using sed (more reliable than envsubst)
+sed -e "s|@BACKEND_HOST@|$BACKEND_HOST|g" \
+    -e "s|@BACKEND_HOST_DESPACHOS@|$BACKEND_HOST_DESPACHOS|g" \
+    /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+
+echo "Generated nginx config:"
+cat /etc/nginx/conf.d/default.conf
+
+# Start nginx
+exec nginx -g "daemon off;"
